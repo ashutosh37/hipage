fcb0437dc1ace77772fdad1bc4afd0e3eed6fd30
asher nig <ashutosh.titan@gmail.com>
2015-01-22 21:59:10 +1100

    added new landing view

--- js/app.js
+++ js/app.js
@@ -94,9 +94,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
-  });
+  })
+      .state('landing', {
+          url: '/landing',
+                  templateUrl: 'templates/landing.html',
+                  controller: 'landingCtrl'
+
+      });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/landing');
 
 });
